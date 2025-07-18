8377c99cb206c3b8b1c4a84374407842ebce785d
akntheartisan <aravinthkumaran410@gmail.com>
2025-07-18 17:10:13 +0530

    webhook checking

--- backend/controller/webhookController.js
+++ backend/controller/webhookController.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const userModel = require('../model/UserRegisterModel')
+const nodemailer = require('nodemailer');
 
 exports.notification = async(req,res)=>{
     console.log('refundnofification',JSON.stringify(req.body));
+
+    async (options) => {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'aravinthkumaran410@gmail.com',
+          pass: 'aioy tkzr gazv atah',
+        },
+      });
+    
+      const mailOptions = {
+        from: 'aravinthkumaran410@gmail.com',
+        to: 'aravinthkumaran410@gmail.com',
+        subject: 'webhooks works perfectly',
+        text: JSON.stringify(req.body),
+      };
+      
+      await transporter.sendMail(mailOptions);
+    };
     
 }
