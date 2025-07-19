fc3799c3f096cbfc2bcb452a9acee7cd68275723
akntheartisan <aravinthkumaran410@gmail.com>
2025-07-19 10:06:11 +0530

    webhook object created

--- backend/controller/webhookController.js
+++ backend/controller/webhookController.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const userModel = require('../model/UserRegisterModel')
-const nodemailer = require('nodemailer');
+const express = require("express");
+const userModel = require("../model/UserRegisterModel");
+const nodemailer = require("nodemailer");
 
-exports.notification = async(req,res)=>{
-    console.log('refundnofification',JSON.stringify(req.body));
+exports.notification = async (req, res) => {
+  console.log("refundnofification", JSON.stringify(req.body));
 
-    async (options) => {
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'aravinthkumaran410@gmail.com',
-          pass: 'aioy tkzr gazv atah',
-        },
-      });
-    
-      const mailOptions = {
-        from: 'aravinthkumaran410@gmail.com',
-        to: 'aravinthkumaran410@gmail.com',
-        subject: 'webhooks works perfectly',
-        text: JSON.stringify(req.body),
-      };
-      
-      await transporter.sendMail(mailOptions);
+  const webhookObject = JSON.stringify(req.body)
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "aravinthkumaran410@gmail.com",
+        pass: "aioy tkzr gazv atah",
+      },
+    });
+
+    const mailOptions = {
+      from: "aravinthkumaran410@gmail.com",
+      to: "aravinthkumaran410@gmail.com",
+      subject: "webhooks works perfectly",
+      text: webhookObject,
     };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
     
-}
+  }
+};
